f0769077248ac913c5cf13f4044d71ad940f2291
JoaoSant7 <pedro_romano7@proton.me>
2026-04-25 14:42:28 -0300

    develop: adicionar classes de professor e aluno com validadores de email, matrícula e telefone

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,68 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+# Classes
+
+
+class Professor(models.Model):
+	nome_professor = models.CharField(max_length=30, verbose_name="Primeiro Nome")
+	sobrenome_professor = models.CharField(max_length=30, verbose_name="Segundo Nome")
+	matricula_professor = models.CharField(
+		max_length=10,
+		validators=[
+			RegexValidator(
+				regex=r"\d{10}$",
+				message="A matrícula deve conter exatamente 10 dígitos",
+			)
+		],
+	)
+	email_professor = models.EmailField(
+		max_length=50,
+		validators=[
+			RegexValidator(
+				regex=r"^[\w\.-]+@edu\.pe\.senac\.br$",
+				message="Use um email institucional da Faculdade Senac.",
+			)
+		],
+	)
+	telefone_professor = models.CharField(
+		max_length=11,
+		validators=[
+			RegexValidator(
+				regex=r"\d{11}$",
+				message="O número de telefone do usuário deve conter 11 dígitos",
+			)
+		],
+	)
+
+
+class Aluno(models.Model):
+	nome_aluno = models.CharField(max_length=30)
+	sobrenome_aluno = models.CharField(max_length=30)
+	matricula_aluno = models.CharField(
+		max_length=10,
+		validators=[
+			RegexValidator(
+				regex=r"\d{10}$",
+				message="A matrícula deve conter exatamente 10 dígitos",
+			)
+		],
+	)
+	email_aluno = models.EmailField(
+		max_length=50,
+		validators=[
+			RegexValidator(
+				regex=r"^[\w\.-]+@edu\.pe\.senac\.br$",
+				message="Use um email institucional da Faculdade Senac.",
+			)
+		],
+	)
+	telefone_aluno = models.CharField(
+		max_length=11,
+		validators=[
+			RegexValidator(
+				regex=r"\d{11}$",
+				message="O número de telefone do usuário deve conter 11 dígitos",
+			)
+		],
+	)
