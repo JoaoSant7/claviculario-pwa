3e797ad1e4d3bc0cb61cd0e876303844a9b2beef
JoaoSant7 <pedro_romano7@proton.me>
2026-04-25 09:15:58 -0300

    adicionar pasta de requisitos; adicionar apps primários

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,13 @@ INSTALLED_APPS = [
 	"django.contrib.messages",
 	"django.contrib.staticfiles",
 	"core",
+	"usuarios",
+	"salas",
+	"chaves",
+	"autorizacoes",
+	"operacoes",
+	"eventos",
+	"relatorios",
 ]
 
 MIDDLEWARE = [
