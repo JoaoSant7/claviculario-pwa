5b83eea4a7b7d5fa02990ffec0a18cef7174ab1b
JoaoSant7 <pedro_romano7@proton.me>
2026-04-25 21:28:42 -0300

    fix: adicionar classe usuario abstrata; corrigir erros de sintaxe; adicionar campos unique

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,23 +1,23 @@
 from django.core.validators import RegexValidator
 from django.db import models
 
-# Classes
 
-
-class Professor(models.Model):
-	nome_professor = models.CharField(max_length=30, verbose_name="Primeiro Nome")
-	sobrenome_professor = models.CharField(max_length=30, verbose_name="Segundo Nome")
-	matricula_professor = models.CharField(
+class Usuario(models.Model):
+	nome = models.CharField(max_length=30, verbose_name="Primeiro Nome")
+	sobrenome = models.CharField(max_length=30, verbose_name="Sobrenome")
+	matricula = models.CharField(
 		max_length=10,
+		unique=True,
 		validators=[
 			RegexValidator(
-				regex=r"\d{10}$",
+				regex=r"^\d{10}$",
 				message="A matrícula deve conter exatamente 10 dígitos",
 			)
 		],
 	)
-	email_professor = models.EmailField(
+	email = models.EmailField(
 		max_length=50,
+		unique=True,
 		validators=[
 			RegexValidator(
 				regex=r"^[\w\.-]+@edu\.pe\.senac\.br$",
@@ -25,44 +25,38 @@ class Professor(models.Model):
 			)
 		],
 	)
-	telefone_professor = models.CharField(
+	telefone = models.CharField(
 		max_length=11,
 		validators=[
 			RegexValidator(
-				regex=r"\d{11}$",
+				regex=r"^\d{11}$",
 				message="O número de telefone do usuário deve conter 11 dígitos",
 			)
 		],
 	)
 
+	class Meta:
+		abstract = True
+		verbose_name = "Usuário"
+		verbose_name_plural = "Usuários"
+		ordering = ["nome", "sobrenome"]
 
-class Aluno(models.Model):
-	nome_aluno = models.CharField(max_length=30)
-	sobrenome_aluno = models.CharField(max_length=30)
-	matricula_aluno = models.CharField(
+	def __str__(self):
+		return f"{self.nome} - {self.matricula}"
+
+
+class Aluno(Usuario):
+	class TurmaChoices(models.TextChoices):
+		ADS039 = "ADS039", "ADS 039"
+		ADS040 = "ADS040", "ADS 040"
+		ADS041 = "ADS041", "ADS 041"
+
+	turma = models.CharField(
 		max_length=10,
-		validators=[
-			RegexValidator(
-				regex=r"\d{10}$",
-				message="A matrícula deve conter exatamente 10 dígitos",
-			)
-		],
+		choices=TurmaChoices.choices,
+		verbose_name="Turma",
 	)
-	email_aluno = models.EmailField(
-		max_length=50,
-		validators=[
-			RegexValidator(
-				regex=r"^[\w\.-]+@edu\.pe\.senac\.br$",
-				message="Use um email institucional da Faculdade Senac.",
-			)
-		],
-	)
-	telefone_aluno = models.CharField(
-		max_length=11,
-		validators=[
-			RegexValidator(
-				regex=r"\d{11}$",
-				message="O número de telefone do usuário deve conter 11 dígitos",
-			)
-		],
-	)
+
+
+class Professor(Usuario):
+	materias = models.CharField(max_length=100, verbose_name="Matéria")
