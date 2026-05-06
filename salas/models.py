ca6ab4fc1220370460d018e0219f6db522f8b102
JoaoSant7 <pedro_romano7@proton.me>
2026-05-06 15:50:38 -0300

    add: criar classe sala no app salas

--- salas/models.py
+++ salas/models.py
@@ -1,3 +1,23 @@
+import uuid
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+# Sala
+###  - id (UUID)
+#   - codigo (CharField, unique)  ← ex: "301A"
+#  - andar (IntegerField)
+#  - descricao (CharField)
+
+
+class Sala(models.Model):
+	andar = models.PositiveSmallIntegerField(
+		validators=[MinValueValidator(0), MaxValueValidator(22)], verbose_name="Andar"
+	)
+	numero = models.PositiveSmallIntegerField(
+		validators=[MinValueValidator(1), MaxValueValidator(6)]
+	)
+	descricao = models.CharField(max_length=100)
+
+	@property
+	def codigo(self):
+		return f"{self.andar}{self.numero:02d}"
