2eccf9ca213dbc5fde07356874bfc3214e0b7c54
Ana Beatriz <anabeatrizdasilva263@gmail.com>
2026-05-15 20:41:35 +0000

    ajustando a descrições das salas

--- salas/models.py
+++ salas/models.py
@@ -12,9 +12,14 @@ class Sala(models.Model):
 	andar = models.PositiveSmallIntegerField(
 		validators=[MinValueValidator(0), MaxValueValidator(22)], verbose_name="Andar"
 	)
-	
-	numero = models.CharField(max_length=4)
-	descricao = models.CharField(max_length=100)
+
+	numero = models.CharField(max_length=4, verbose_name="Número da Sala")
+
+	descricao = models.CharField(
+		max_length=100,
+		verbose_name="Descrição",
+		help_text="MAaqueteria, Inovação, etc.",
+		)
 
 	@property
 	def codigo(self):
