e5f892fed6ad8bb2872271531c139a37a631caa8
Eloisa de Andrade 🌱 <eloisaandrade1006@gmail.com>
2026-06-15 17:44:57 -0300

    feat: implementar backend completo do claviculario

--- operacoes/forms.py
+++ operacoes/forms.py
@@ -1,14 +1,13 @@
 from django import forms
-from .models import Emprestimo, Devolucao
+
+from chaves.models import Chave
+from usuarios.models import Usuario
 
 
-class EmprestimoForm(forms.ModelForm):
-    class Meta:
-        model = Emprestimo
-        fields = ["usuario", "chave"]
+class RetiradaForm(forms.Form):
+	usuario = forms.ModelChoiceField(queryset=Usuario.objects.filter(ativo=True))
+	chave = forms.ModelChoiceField(queryset=Chave.objects.all())
 
 
-class DevolucaoForm(forms.ModelForm):
-    class Meta:
-        model = Devolucao
-        fields = ["emprestimo"]
+class DevolucaoForm(forms.Form):
+	chave = forms.ModelChoiceField(queryset=Chave.objects.all())
