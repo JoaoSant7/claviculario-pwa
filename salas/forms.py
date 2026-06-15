63ba6d19caa1d8356fa495a048500b3960173347
Eloisa de Andrade 🌱 <eloisaandrade1006@gmail.com>
2026-06-15 16:55:21 -0300

    feat: adicionar form de Sala

---
+++ salas/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Sala
+
+
+class SalaForm(forms.ModelForm):
+    class Meta:
+        model = Sala
+        fields = ["andar", "numero", "tipo_sala", "descricao"]
