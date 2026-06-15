d2bb51eb68e6eb5a752bd213c8cb5a61c7e6dc50
Eloisa de Andrade 🌱 <eloisaandrade1006@gmail.com>
2026-06-15 16:50:20 -0300

    feat: configurar admin de Autorizacao

--- autorizacoes/admin.py
+++ autorizacoes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Autorizacao
 
-# Register your models here.
+
+@admin.register(Autorizacao)
+class AutorizacaoAdmin(admin.ModelAdmin):
+    list_display = ["usuario", "sala", "concedida_por", "valida_de", "valida_ate", "ativa"]
