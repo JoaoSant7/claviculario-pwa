cfd1cd7c8f101844380c157d501c4ea225bc0c34
Eloisa de Andrade 🌱 <eloisaandrade1006@gmail.com>
2026-06-15 16:49:36 -0300

    feat: configurar admin de Chave

--- chaves/admin.py
+++ chaves/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Chave
 
-# Register your models here.
+
+@admin.register(Chave)
+class ChaveAdmin(admin.ModelAdmin):
+    list_display = ["numero", "sala", "status", "slot_x", "slot_y", "slot_ocupado"]
