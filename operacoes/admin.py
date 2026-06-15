f3e918d03bc5c7ce37b7b73bbc8df5720144678a
Eloisa de Andrade 🌱 <eloisaandrade1006@gmail.com>
2026-06-15 17:41:46 -0300

    feat: implementar backend completo do claviculario

--- operacoes/admin.py
+++ operacoes/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
-from .models import Emprestimo, Devolucao
+
+from .models import Devolucao, Emprestimo, Operacao
+
+
+@admin.register(Operacao)
+class OperacaoAdmin(admin.ModelAdmin):
+	list_display = ("id", "usuario", "chave")
+	search_fields = ("usuario__matricula", "chave__numero", "chave__sala__codigo")
 
 
 @admin.register(Emprestimo)
 class EmprestimoAdmin(admin.ModelAdmin):
-    list_display = ["usuario", "chave", "retirado_em", "limite_devolucao", "atraso_registrado"]
+	list_display = ("id", "usuario", "chave", "retirado_em", "devolvido_em", "atraso_registrado")
+	list_filter = ("devolvido_em", "atraso_registrado")
+	search_fields = ("usuario__matricula", "chave__numero", "chave__sala__codigo")
+	readonly_fields = ("retirado_em",)
 
 
 @admin.register(Devolucao)
 class DevolucaoAdmin(admin.ModelAdmin):
-    list_display = ["emprestimo", "feito_em"]
+	list_display = ("id", "emprestimo", "feito_em")
+	readonly_fields = ("feito_em",)
