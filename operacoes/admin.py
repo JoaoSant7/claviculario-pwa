031c92612b314d9f6742d3295eabf0179e8abdc5
Eloisa de Andrade 🌱 <eloisaandrade1006@gmail.com>
2026-06-15 16:52:18 -0300

    feat: configurar admin de Emprestimo e Devolucao

--- operacoes/admin.py
+++ operacoes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Emprestimo, Devolucao
 
-# Register your models here.
+
+@admin.register(Emprestimo)
+class EmprestimoAdmin(admin.ModelAdmin):
+    list_display = ["usuario", "chave", "retirado_em", "limite_devolucao", "atraso_registrado"]
+
+
+@admin.register(Devolucao)
+class DevolucaoAdmin(admin.ModelAdmin):
+    list_display = ["emprestimo", "feito_em"]
